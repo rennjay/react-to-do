b6161b7625f7839fe063264c1732596e810a0ea5
Rennjay Soterio <rennjay.soterio@betterask.erni>
2023-10-30 22:10:51 +0800

    Set description field to smaller

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -4,7 +4,7 @@ const TodoForm = ({handleTodoAdd, handleTitleChange, title, handleDescriptionCha
         <form id='TodoForm' className="flex flex-col justify-items-stretch" onSubmit={!isEditMode ? handleTodoAdd : handleTaskUpdate}>
             <h1 className="mb-3 text-center text-lg mt-3 font-semibold">{(!isEditMode ? 'Add':'Edit') + ' To-do'}</h1>
             <input ref={titleField} onChange={handleTitleChange} value={title} type="text" name="title" id="title" placeholder="Title" className="mb-3 drop-shadow p-2"/>
-            <textarea onChange={handleDescriptionChange} value={description} name="description" id="description" cols="30" rows="10" placeholder="Description" className="mb-3 drop-shadow p-2"></textarea>
+            <textarea onChange={handleDescriptionChange} value={description} name="description" id="description" cols="30" rows="10" placeholder="Description" className="mb-3 drop-shadow p-2 text-sm"></textarea>
             <button type="submit" className="bg-secondary py-1 text-white text-l">Add</button>
             {isEditMode ? <button onClick={handleCancelEdit} className="bg-tertiary py-1 text-white text-l mt-3">Cancel</button> : null}
         </form>
