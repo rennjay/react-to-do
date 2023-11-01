ef2114ac13d494aa9bd093ffa9abefee34293f94
Rennjay Soterio <rennjay.soterio@betterask.erni>
2023-11-01 12:48:32 +0800

    Adjust some formats using prettier

--- src/components/TaskControls.js
+++ src/components/TaskControls.js
@@ -1,12 +1,28 @@
-import { RiDeleteBinLine, RiEditBoxLine } from 'react-icons/ri';
+import { RiDeleteBinLine, RiEditBoxLine } from "react-icons/ri";
 
-const TaskControls = function({showTaskControls, handleTaskDelete, handleTaskEdit, todo}) {
-    return (
-        <div className={'task-controls flex justify-items-center items-center px-2 gap-1.5 ' + ( showTaskControls ? 'block':'hidden')}>
-            <RiEditBoxLine className='text-gray-400 text-2xl z-10' onClick={() => handleTaskEdit(todo)}/>
-            <RiDeleteBinLine className='text-gray-400 text-2xl z-10' onClick={() => handleTaskDelete(todo.id)}/>
-        </div>
-    );
-}
+const TaskControls = function ({
+  showTaskControls,
+  handleTaskDelete,
+  handleTaskEdit,
+  todo,
+}) {
+  return (
+    <div
+      className={
+        "task-controls flex justify-items-center items-center px-2 gap-1.5 " +
+        (showTaskControls ? "block" : "hidden")
+      }
+    >
+      <RiEditBoxLine
+        className="text-gray-400 text-2xl z-10"
+        onClick={() => handleTaskEdit(todo)}
+      />
+      <RiDeleteBinLine
+        className="text-gray-400 text-2xl z-10"
+        onClick={() => handleTaskDelete(todo.id)}
+      />
+    </div>
+  );
+};
 
 export default TaskControls;
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,36 +1,65 @@
-import { FaCheckCircle } from 'react-icons/fa';
-import TaskControls from './TaskControls';
-import { useState } from 'react';
+import { FaCheckCircle } from "react-icons/fa";
+import TaskControls from "./TaskControls";
+import { useState } from "react";
 
-function TaskItem({todo, handleCompleteTodo, handleTaskDelete, handleTaskEdit}){
-let [showTaskControls, setShowTaskControls] = useState(false);
+function TaskItem({
+  todo,
+  handleCompleteTodo,
+  handleTaskDelete,
+  handleTaskEdit,
+}) {
+  let [showTaskControls, setShowTaskControls] = useState(false);
 
-const handleMouseOver = (e) =>    {
+  const handleMouseOver = (e) => {
     setShowTaskControls(true);
-};
+  };
 
-const handleMouseLeave = (e) => {
+  const handleMouseLeave = (e) => {
     setShowTaskControls(false);
-};
-    return(
-        <div className="flex task-item-container py-3 justify-between"
-            onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
-            <div className="task-item-content flex w-full" onClick={(event) => handleCompleteTodo(event, todo)}>
-                <div className="flex justify-items-center items-center px-2">
-                    <FaCheckCircle className={'text-2xl ' + (todo.isCompleted ? 'text-secondary' : 'text-gray-400')} />
-                </div>
-                <div>
-                    <div className="TaskItemHeader flex">
-                        <span className= { todo.isCompleted ? 'line-through':''}>{todo.title}</span>
-                    </div>
-                    <div className="task-item-description text-xs">
-                        <p>{todo.description}</p>
-                        {todo.isCompleted ? <p className='text-xs text-gray-400 italic'>Completed on {(new Date(todo.completionDate)).toLocaleString()}</p>:''}
-                    </div>
-                </div>
-            </div>
-            <TaskControls showTaskControls={showTaskControls} todo={todo} handleTaskDelete={handleTaskDelete} handleTaskEdit={handleTaskEdit}/>
+  };
+  return (
+    <div
+      className="flex task-item-container py-3 justify-between"
+      onMouseOver={handleMouseOver}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div
+        className="task-item-content flex w-full"
+        onClick={(event) => handleCompleteTodo(event, todo)}
+      >
+        <div className="flex justify-items-center items-center px-2">
+          <FaCheckCircle
+            className={
+              "text-2xl " +
+              (todo.isCompleted ? "text-secondary" : "text-gray-400")
+            }
+          />
         </div>
-    );
+        <div>
+          <div className="TaskItemHeader flex">
+            <span className={todo.isCompleted ? "line-through" : ""}>
+              {todo.title}
+            </span>
+          </div>
+          <div className="task-item-description text-xs">
+            <p>{todo.description}</p>
+            {todo.isCompleted ? (
+              <p className="text-xs text-gray-400 italic">
+                Completed on {new Date(todo.completionDate).toLocaleString()}
+              </p>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      </div>
+      <TaskControls
+        showTaskControls={showTaskControls}
+        todo={todo}
+        handleTaskDelete={handleTaskDelete}
+        handleTaskEdit={handleTaskEdit}
+      />
+    </div>
+  );
 }
 export default TaskItem;
